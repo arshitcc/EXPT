63d28d16ee35437a5828f2051fde42f9504a54b7
arshitcc <arshitcc@gmail.com>
2024-07-09 06:48:27 +0530

    02

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
 
   const authStatus = useSelector((state) =>  state.auth.authStatus);
 
-  return authStatus ? (<div>Welcome User</div>) : (<div>Please Login</div>)
+  return authStatus ? (<div className='text-center text-8xl'>Welcome User</div>) : (<div className='text-center text-8xl'>Please Login</div>)
 }
 
 export default Home
