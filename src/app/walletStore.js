27eba666dee7095fbee9e888fa75491767331bf3
arshitcc <arshitcc@gmail.com>
2024-07-10 02:35:19 +0530

    x

--- src/app/walletStore.js
+++ src/app/walletStore.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../features/authSlice";
-import paymentSlice from "../features/paymentSlice";
 
 export const myStore = configureStore({
     reducer : {
