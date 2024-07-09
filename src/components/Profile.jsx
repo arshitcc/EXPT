534f0974b2c347343dcd21e171bc25173441c72e
arshitcc <arshitcc@gmail.com>
2024-07-09 06:58:40 +0530

    04

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,13 +9,13 @@ function Profile() {
 
     <div className='bg-gray-900'>
         <div className='max-w-2xl mx-auto sm:max-w-screen-lg bg-gray-800 shadow-xl rounded-lg text-gray-900 '>
-            <div className='grid grid-cols-3 p-4 items-center'>
-                <div className='col-span-1 '>
+            <div className='grid grid-cols-1 md:grid-cols-3 p-4 items-center'>
+                <div className='md:col-span-1 m-4'>
                     <div className="ml-4 w-64 relative border-4 border-white rounded-full overflow-hidden">
                         <img className="object-cover object-center h-64" src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='Woman looking front'/>
                     </div>
                 </div>
-                <div className='col-span-2'>
+                <div className='md:col-span-2 m-4'>
                     <div className="flow-root py-3 shadow-sm">
                         <dl className="divide-y divide-gray-100 text-sm">
                             <div className="grid grid-cols-1 gap-1 p-3 bg-white sm:grid-cols-3 sm:gap-4">
